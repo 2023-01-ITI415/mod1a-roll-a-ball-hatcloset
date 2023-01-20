d5b2e37859823eb3cf08b805f357b402caecc7d2
Tankakuka <officialtankakuka@gmail.com>
2023-01-19 20:40:45 -0500

    Added InputActions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<rigidbody>();
+    }
+
+    void onMove(inputValue movementValue)
+    {
+        // function body
+        Vector2 movementVector = movementValue.get<vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movementVector);
+    }
+}
